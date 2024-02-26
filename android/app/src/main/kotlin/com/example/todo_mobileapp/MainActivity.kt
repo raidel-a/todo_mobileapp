fa99f5f460d2bde4c341fb9494b6f4ce2f14d0de
raihven <raihven@protonmail.com>
2024-02-25 20:49:03 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/todo_mobileapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_mobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/app_str.dart
@@ -0,0 +1,17 @@
+class AppStr {
+  static const String mainTitle = "My Tasks";
+  static const String deletedTask = "Task was deleted";
+  static const String doneAllTask = "All Tasks Completed";
+  static const String addNewTask = "Add New ";
+  static const String updateCurrentTask = "Updadte ";
+  static const String taskString = "Task";
+  static const String titleOfTitleTextField = "What's the Plan";
+  static const String addNote = "Add Note";
+  static const String timeString = "Time";
+  static const String dateString = "Date";
+  static const String deleteTask = "Delete Task";
+  static const String addTaskString = "Add Task";
+  static const String updateTaskStirng = "Update Task";
+  static const String oopsMsg = "Ooops!";
+  static const String areYouSure = "Are You Sure?";
+}
